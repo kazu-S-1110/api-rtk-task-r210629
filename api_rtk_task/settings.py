dddbcb214c69ba8e05e6becd1b0cef9bdc8e482f
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-01 12:59:53 +0900

    django_heroku installed

--- api_rtk_task/settings.py
+++ api_rtk_task/settings.py
@@ -104,6 +104,8 @@ DATABASES = {
     }
 }
 
+import django_heroku
+django_heroku.settings(locals())
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
